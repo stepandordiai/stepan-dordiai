62fb524c4a97d5ba38d39a7c0dd5d3a959f910a1
Stepan Dordiai <stepandordiai@gmail.com>
2025-04-19 17:27:12 +0200

    v8.0.4 Update about with space after each word

--- src/components/textLine/TextLine.jsx
+++ src/components/textLine/TextLine.jsx
@@ -1,11 +1,11 @@
 import "./TextLine.scss";
 
 const TextLine = (props) => {
-    return (
-        <div className="text-line-container">
-            <div className="text-line">{props.children}</div>
-        </div>
-    );
+	return (
+		<div className="text-line-container">
+			<div className="text-line">{props.children}</div>
+		</div>
+	);
 };
 
 export default TextLine;
--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,29 +1,31 @@
 import { Helmet } from "react-helmet";
+import React from "react";
 import AnimateElements from "../../utils/AnimateElements";
 import TextLine from "../../components/textLine/TextLine";
 import "./About.scss";
 
 function About() {
-    AnimateElements(".text-line", "revealTextLine 1s forwards", 50);
+	AnimateElements(".text-line", "revealTextLine 1s forwards", 50);
 
-    return (
-        <>
-            <Helmet>
-                <title>ABOUT | STEPAN DORDIAI</title>
-            </Helmet>
-            <section className="about">
-                {`Self-taught Web developer with more than 2 years of diverse experience in designing and building responsive web pages and applications. Constantly seeking for new challenges and opportunities that will expand my skill set`
-                    .split(` `)
-                    .map((word, index) => {
-                        return (
-                            <TextLine key={index}>
-                                <p style={{ marginLeft: "5px" }}>{word}</p>
-                            </TextLine>
-                        );
-                    })}
-            </section>
-        </>
-    );
+	return (
+		<>
+			<Helmet>
+				<title>ABOUT | STEPAN DORDIAI</title>
+			</Helmet>
+			<section className="about">
+				{`Self-taught Web developer with more than 2 years of diverse experience in designing and building responsive web pages and applications. Constantly seeking for new challenges and opportunities that will expand my skill set`
+					.split(" ")
+					.map((word, index) => {
+						return (
+							<React.Fragment key={index}>
+								<TextLine>{word}</TextLine>
+								<span> </span>
+							</React.Fragment>
+						);
+					})}
+			</section>
+		</>
+	);
 }
 
 export default About;
