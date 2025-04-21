8f05fcfa945e89151184f333e2e33583c1bc9062
Stepan Dordiai <stepandordiai@gmail.com>
2025-04-21 18:10:18 +0200

    v8.1.0

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,36 +1,38 @@
 import AnimateElements from "../../utils/AnimateElements";
-import { makeCursorActive, makeCursorNormal } from "../../utils/cursorState";
 import { NavLink } from "react-router-dom";
+import { interactCursor, removeInteractCursor } from "../../utils/cursorState";
 import "./Project.scss";
 
 function Project({ id, title, index, projectImg }) {
-    AnimateElements(".project", "revealProject 1s forwards");
+	AnimateElements(".project", "revealProject 1s forwards");
 
-    return (
-        <div
-            className="project js-project"
-            onMouseEnter={() => makeCursorActive(title, id)}
-            onMouseLeave={makeCursorNormal}
-        >
-            <NavLink
-                onClick={makeCursorNormal}
-                className="project__link"
-                to={`/project-page/${id}`}
-            ></NavLink>
-            <p className="project__number">
-                {index + 1 < 10 ? `0${index + 1}` : index + 1}
-            </p>
-            <div className="right-side">
-                <p className="project__title">{title}</p>
-                <img
-                    className="project__img"
-                    src={projectImg}
-                    alt={title}
-                    loading="lazy"
-                />
-            </div>
-        </div>
-    );
+	return (
+		<div
+			onMouseEnter={interactCursor}
+			onMouseLeave={removeInteractCursor}
+			onMouseDown={removeInteractCursor}
+			onMouseUp={interactCursor}
+			className="project js-project"
+		>
+			<NavLink
+				onClick={removeInteractCursor}
+				className="project__link"
+				to={`/project-page/${id}`}
+			></NavLink>
+			<p className="project__number">
+				{index + 1 < 10 ? `0${index + 1}` : index + 1}
+			</p>
+			<div className="right-side">
+				<p className="project__title">{title}</p>
+				<img
+					className="project__img"
+					src={projectImg}
+					alt={title}
+					loading="lazy"
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default Project;
