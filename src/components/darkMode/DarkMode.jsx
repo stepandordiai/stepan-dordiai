3515dfd84240ceda49ed9dc50c7701567e4bfe04
Stepan Dordiai <stepandordiai@gmail.com>
2025-04-21 18:22:00 +0200

    v8.1.1 Fix bugs

--- src/components/darkMode/DarkMode.jsx
+++ src/components/darkMode/DarkMode.jsx
@@ -1,103 +1,68 @@
 import { useLocalStorage } from "../../utils/UseLocalStorage";
 import { useEffect, useRef } from "react";
-import {
-    makeCursorActive,
-    makeCursorNormal,
-    showText,
-} from "../../utils/cursorState";
+import { interactCursor, removeInteractCursor } from "../../utils/cursorState";
 import Resume from "../resume/Resume";
 import "./DarkMode.scss";
 
 function DarkMode() {
-    const [darkMode, setDarkMode] = useLocalStorage("darkMode", "light");
+	const [darkMode, setDarkMode] = useLocalStorage("darkMode", "light");
 
-    const refBtn = useRef(null);
+	const refBtn = useRef(null);
 
-    useEffect(() => {
-        const variables = document.querySelector(":root");
+	useEffect(() => {
+		const variables = document.querySelector(":root");
 
-        if (darkMode === "light") {
-            document.body.style.backgroundColor = "#fff";
-            variables.style.setProperty("--bg-clr", "var(--bg-clr-light)");
-            variables.style.setProperty("--txt-clr", "var(--txt-clr-dark)");
-            variables.style.setProperty(
-                "--semi-tp-05",
-                "var(--semi-tp-05-dark)"
-            );
-            variables.style.setProperty(
-                "--semi-tp-10",
-                "var(--semi-tp-10-dark)"
-            );
-            variables.style.setProperty(
-                "--semi-tp-25",
-                "var(--semi-tp-25-dark)"
-            );
-            variables.style.setProperty(
-                "--semi-tp-50",
-                "var(--semi-tp-50-dark)"
-            );
-            variables.style.setProperty(
-                "--bg-semi-tp-50",
-                "var(--bg-semi-tp-50-light)"
-            );
-            refBtn.current.textContent = "DARK";
-        } else {
-            document.body.style.backgroundColor = "#000";
-            variables.style.setProperty("--bg-clr", "var(--bg-clr-dark)");
-            variables.style.setProperty("--txt-clr", "var(--txt-clr-light)");
-            variables.style.setProperty(
-                "--semi-tp-05",
-                "var(--semi-tp-05-light)"
-            );
-            variables.style.setProperty(
-                "--semi-tp-10",
-                "var(--semi-tp-10-light)"
-            );
-            variables.style.setProperty(
-                "--semi-tp-25",
-                "var(--semi-tp-25-light)"
-            );
-            variables.style.setProperty(
-                "--semi-tp-50",
-                "var(--semi-tp-50-light)"
-            );
-            variables.style.setProperty(
-                "--bg-semi-tp-50",
-                "var(--bg-semi-tp-50-dark)"
-            );
-            refBtn.current.textContent = "LIGHT";
-        }
-    }, [darkMode]);
+		if (darkMode === "light") {
+			document.body.style.backgroundColor = "#fff";
+			variables.style.setProperty("--bg-clr", "var(--bg-clr-light)");
+			variables.style.setProperty("--txt-clr", "var(--txt-clr-dark)");
+			variables.style.setProperty("--semi-tp-05", "var(--semi-tp-05-dark)");
+			variables.style.setProperty("--semi-tp-10", "var(--semi-tp-10-dark)");
+			variables.style.setProperty("--semi-tp-25", "var(--semi-tp-25-dark)");
+			variables.style.setProperty("--semi-tp-50", "var(--semi-tp-50-dark)");
+			variables.style.setProperty(
+				"--bg-semi-tp-50",
+				"var(--bg-semi-tp-50-light)"
+			);
+			refBtn.current.textContent = "DARK";
+		} else {
+			document.body.style.backgroundColor = "#000";
+			variables.style.setProperty("--bg-clr", "var(--bg-clr-dark)");
+			variables.style.setProperty("--txt-clr", "var(--txt-clr-light)");
+			variables.style.setProperty("--semi-tp-05", "var(--semi-tp-05-light)");
+			variables.style.setProperty("--semi-tp-10", "var(--semi-tp-10-light)");
+			variables.style.setProperty("--semi-tp-25", "var(--semi-tp-25-light)");
+			variables.style.setProperty("--semi-tp-50", "var(--semi-tp-50-light)");
+			variables.style.setProperty(
+				"--bg-semi-tp-50",
+				"var(--bg-semi-tp-50-dark)"
+			);
+			refBtn.current.textContent = "LIGHT";
+		}
+	}, [darkMode]);
 
-    const changeMode = () => {
-        setDarkMode((currentMode) => {
-            return currentMode === "dark" ? "light" : "dark";
-        });
+	const toggleDarkMode = () => {
+		setDarkMode((currentMode) => {
+			return currentMode === "dark" ? "light" : "dark";
+		});
+	};
 
-        // I added setTimeout to change text after a function was completed
-        setTimeout(() => {
-            document.querySelector(".text p").textContent =
-                refBtn.current.textContent;
-            showText();
-        }, 0);
-    };
-
-    return (
-        <div className="dark-mode">
-            <Resume />
-            <button
-                ref={refBtn}
-                className="dark-mode__btn"
-                onClick={changeMode}
-                onMouseEnter={() =>
-                    makeCursorActive(refBtn.current.textContent)
-                }
-                onMouseLeave={makeCursorNormal}
-            >
-                DARK
-            </button>
-        </div>
-    );
+	return (
+		<div className="dark-mode">
+			<Resume />
+			<button
+				onMouseEnter={interactCursor}
+				onMouseLeave={removeInteractCursor}
+				onMouseDown={removeInteractCursor}
+				onMouseUp={interactCursor}
+				ref={refBtn}
+				className="dark-mode__btn"
+				onClick={toggleDarkMode}
+			>
+				DARK
+			</button>
+		</div>
+	);
 }
 
 export default DarkMode;
--- src/pages/playground/Playground.jsx
+++ src/pages/playground/Playground.jsx
@@ -1,76 +1,44 @@
 import { Helmet } from "react-helmet";
-// import axios from "axios";
 import pgData from "./../../data/pgData.json";
 import AnimateElements from "../../utils/AnimateElements";
 import ScrollBtn from "../../components/scrollBtn/ScrollBtn";
-import { makeCursorActive, makeCursorNormal } from "../../utils/cursorState";
-// import { interactCursor, removeInteractCursor } from "../../utils/cursorState";
+import { interactCursor, removeInteractCursor } from "../../utils/cursorState";
 import "./Playground.scss";
 
 const Playground = () => {
-    // const [projects, setProjects] = useState([]);
+	AnimateElements(".pg-project", "revealPgProject 1s forwards");
 
-    // useEffect(() => {
-    //     const awaiting = document.createElement("p");
-    //     awaiting.classList.add("awaiting");
-    //     awaiting.textContent =
-    //         "Loading data...(it may take some time (30-60sec) due the inactivity of the server (like 30min), after it loads immediately)";
-    //     document.querySelector(".js-pg-section").appendChild(awaiting);
-    //     let isError = false;
-    //     axios
-    //         .get("https://stepan-dordiai-backend.onrender.com")
-    //         .then((projects) => setProjects(projects.data))
-    //         .catch((error) => {
-    //             // TODO: Write custom error
-    //             awaiting.textContent = error.message;
-    //             awaiting.style.animation = "none";
-    //             isError = true;
-    //         })
-    //         .finally(() => {
-    //             if (!isError) {
-    //                 awaiting.remove();
-    //             } else {
-    //                 isError = false;
-    //                 return;
-    //             }
-    //         });
-    // }, []);
-
-    AnimateElements(".pg-project", "revealPgProject 1s forwards");
-
-    return (
-        <>
-            <Helmet>
-                <title>PLAYGROUND | STEPAN DORDIAI</title>
-            </Helmet>
-            <section className="pg js-pg">
-                {pgData
-                    .map(({ title, liveSite }, index) => {
-                        return (
-                            <a
-                                onMouseEnter={() =>
-                                    makeCursorActive(title, liveSite)
-                                }
-                                onMouseLeave={makeCursorNormal}
-                                className="pg-project"
-                                href={liveSite}
-                                target="_blank"
-                                key={index}
-                            >
-                                <p className="pg-project__number">
-                                    {index + 1 < 10
-                                        ? `00${index + 1}`
-                                        : `0${index + 1}`}
-                                </p>
-                                <p className="pg-project__title">{title}</p>
-                            </a>
-                        );
-                    })
-                    .reverse()}
-            </section>
-            <ScrollBtn className={".js-pg"} />
-        </>
-    );
+	return (
+		<>
+			<Helmet>
+				<title>PLAYGROUND | STEPAN DORDIAI</title>
+			</Helmet>
+			<section className="pg js-pg">
+				{pgData
+					.map(({ title, liveSite }, index) => {
+						return (
+							<a
+								key={index}
+								onMouseEnter={interactCursor}
+								onMouseLeave={removeInteractCursor}
+								onMouseDown={removeInteractCursor}
+								onMouseUp={interactCursor}
+								className="pg-project"
+								href={liveSite}
+								target="_blank"
+							>
+								<p className="pg-project__number">
+									{index + 1 < 10 ? `00${index + 1}` : `0${index + 1}`}
+								</p>
+								<p className="pg-project__title">{title}</p>
+							</a>
+						);
+					})
+					.reverse()}
+			</section>
+			<ScrollBtn className={".js-pg"} />
+		</>
+	);
 };
 
 export default Playground;
