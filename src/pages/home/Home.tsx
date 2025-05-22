b21300d92b997238df180d6f60f50db26028e6df
Stepan Dordiai <stepandordiai@gmail.com>
2025-05-22 16:44:26 +0200

    v8.1.15 Update Home and ProjectPage with tsx

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -6,52 +6,68 @@ import "./Home.scss";
 
 function Home() {
 	function addAnimation() {
-		const scroller = document.querySelector(".scroller");
+		const scroller = document.querySelector(
+			".scroller"
+		) as HTMLDivElement | null;
 
-		scroller.setAttribute("data-animated", true);
+		// setAttribute expects both arguments to be strings
+		scroller?.setAttribute("data-animated", "true");
 
-		const scrollerInner = scroller.querySelector(".scroller__inner");
-		const scrollerContent = Array.from(scrollerInner.children);
+		const scrollerInner = scroller?.querySelector(
+			".scroller__inner"
+		) as HTMLDivElement | null;
+		const scrollerContent = scrollerInner
+			? Array.from(scrollerInner?.children)
+			: [];
 
 		scrollerContent.forEach((item) => {
-			const duplicatedItem = item.cloneNode(true);
+			const duplicatedItem = item.cloneNode(true) as HTMLElement;
 			let textHTML = "";
-			duplicatedItem.innerHTML.split("").forEach((char) => {
+			duplicatedItem.innerHTML.split("").forEach((char: string) => {
 				textHTML += `<span>${char}</span>`;
 			});
 
 			duplicatedItem.innerHTML = textHTML;
-			duplicatedItem.setAttribute("aria-hidden", true);
-			scrollerInner.appendChild(duplicatedItem);
+			duplicatedItem.setAttribute("aria-hidden", "true");
+			scrollerInner?.appendChild(duplicatedItem);
 		});
 	}
 
 	function showText() {
-		const text = document.querySelector(".js-home__title");
-		let textHTML = "";
-		text.innerHTML.split("").forEach((char) => {
-			textHTML += `<span>${char}</span>`;
-		});
+		const text = document.querySelector(
+			".js-home__title"
+		) as HTMLElement | null;
 
-		text.innerHTML = textHTML;
+		if (text) {
+			let textHTML = "";
+			text.innerHTML.split("").forEach((char) => {
+				textHTML += `<span>${char}</span>`;
+			});
+
+			text.innerHTML = textHTML;
+		}
 
 		// Add opacity animation for each char
-		document.querySelectorAll(".js-home__title span").forEach((char, index) => {
-			setTimeout(() => {
-				char.style.animation = "revealChar 1s forwards";
-			}, 100 * index);
-		});
+		document
+			.querySelectorAll<HTMLElement>(".js-home__title span")
+			.forEach((char, index) => {
+				setTimeout(() => {
+					char.style.animation = "revealChar 1s forwards";
+				}, 100 * index);
+			});
 	}
 
 	useEffect(() => {
+		const loading = document.querySelector(
+			".loading"
+		) as HTMLParagraphElement | null;
+
 		if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
-			if (document.querySelector(".loading").style.display === "initial") {
-				document
-					.querySelector(".loading")
-					.addEventListener("animationend", () => {
-						addAnimation();
-						showText();
-					});
+			if (loading?.style.display === "initial") {
+				loading.addEventListener("animationend", () => {
+					addAnimation();
+					showText();
+				});
 			} else {
 				addAnimation();
 				showText();
