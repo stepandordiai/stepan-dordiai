40d7ff4289160884430986f9c78ad3ae25240445
Stepan Dordiai <stepandordiai@gmail.com>
2025-09-08 11:41:25 +0200

    Update skillsData and Skills page

--- src/pages/skills/Skills.tsx
+++ src/pages/skills/Skills.tsx
@@ -2,60 +2,32 @@ import { Helmet } from "react-helmet-async";
 import skillsData from "../../assets/data/skillsData";
 import Skill from "../../components/skill/Skill";
 import ScrollBtn from "../../components/scrollBtn/ScrollBtn";
+import React from "react";
 import "./Skills.scss";
 
 function Skills() {
+	const uniqueSkillTypes = [...new Set(skillsData.map((skill) => skill.type))];
+
 	return (
 		<>
 			<Helmet>
 				<title>SKILLS | STEPAN DORDIAI</title>
 			</Helmet>
 			<div className="skills js-skills">
-				<p className="skills__title">Front-end</p>
-				<div className="skills-grid">
-					{skillsData
-						.filter((skill) => skill.type === "frontend")
-						.reverse()
-						.map((skill) => {
-							return <Skill key={skill.id} skill={skill} />;
-						})}
-				</div>
-				<p className="skills__title">Back-end</p>
-				<div className="skills-grid">
-					{skillsData
-						.filter((skill) => skill.type === "backend")
-						.reverse()
-						.map((skill) => {
-							return <Skill key={skill.id} skill={skill} />;
-						})}
-				</div>
-				<p className="skills__title">Database</p>
-				<div className="skills-grid">
-					{skillsData
-						.filter((skill) => skill.type === "database")
-						.reverse()
-						.map((skill) => {
-							return <Skill key={skill.id} skill={skill} />;
-						})}
-				</div>
-				<p className="skills__title">Design</p>
-				<div className="skills-grid">
-					{skillsData
-						.filter((skill) => skill.type === "design")
-						.reverse()
-						.map((skill) => {
-							return <Skill key={skill.id} skill={skill} />;
-						})}
-				</div>
-				<p className="skills__title">Other</p>
-				<div className="skills-grid">
-					{skillsData
-						.filter((skill) => skill.type === "other")
-						.reverse()
-						.map((skill) => {
-							return <Skill key={skill.id} skill={skill} />;
-						})}
-				</div>
+				{uniqueSkillTypes.map((skillType) => {
+					return (
+						<React.Fragment key={skillType}>
+							<p className="skills__title">{skillType}</p>
+							<div className="skills-grid">
+								{skillsData
+									.filter((skill) => skill.type === skillType)
+									.map((skill) => {
+										return <Skill key={skill.id} skill={skill} />;
+									})}
+							</div>
+						</React.Fragment>
+					);
+				})}
 			</div>
 			<ScrollBtn className={".js-skills"} />
 		</>
