986b67b5e2f068203aff9695f142483362fc063f
Stepan Dordiai <stepandordiai@gmail.com>
2026-01-22 09:44:08 +0100

    refactor: remove lib folder and update Portfolio page

--- src/pages/portfolio/Portfolio.tsx
+++ src/pages/portfolio/Portfolio.tsx
@@ -2,12 +2,44 @@ import { Helmet } from "react-helmet-async";
 import Project from "../../components/project/Project";
 import ScrollBtn from "../../components/scrollBtn/ScrollBtn";
 import { interactCursor, removeInteractCursor } from "../../utils/cursorState";
-import getPortfolioData from "../../lib/api";
 import ProjectInterface from "../../types/Project";
+import { useEffect, useState } from "react";
 import "./Portfolio.scss";
 
-async function Portfolio() {
-	const portfolio: ProjectInterface[] = await getPortfolioData();
+interface ImportMetaEnv {
+	readonly VITE_API_URL: string;
+	readonly VITE_API_KEY: string;
+}
+
+// TODO: learn this
+declare global {
+	interface ImportMeta {
+		readonly env: ImportMetaEnv;
+	}
+}
+
+function Portfolio() {
+	const [portfolio, setPortfolio] = useState<ProjectInterface[]>([]);
+
+	useEffect(() => {
+		async function getPortfolioData() {
+			const res = await fetch(import.meta.env.VITE_API_URL, {
+				headers: {
+					"X-SILO-KEY": import.meta.env.VITE_API_KEY,
+					"Content-Type": "application/json",
+				},
+			});
+
+			if (!res.ok) throw new Error("Network res was not ok");
+
+			const portfolioData = await res.json();
+			setPortfolio(portfolioData);
+		}
+
+		// TODO: learn this
+		getPortfolioData().catch((err) => console.error(err));
+	}, []);
+
 	return (
 		<>
 			<Helmet>
