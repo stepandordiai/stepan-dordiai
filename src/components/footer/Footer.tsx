1d3fad0175dc3d75643157b686b55af231bf5fdd
Stepan Dordiai <stepandordiai@gmail.com>
2026-03-27 22:39:23 +0100

    refactor: update Footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
 	return (
 		<footer className="footer">
 			<p className="copyright">
-				&copy; 2024-{new Date().getFullYear()} STEPAN DORDIAI
+				&copy; {new Date().getFullYear()} STEPAN DORDIAI
 			</p>
 		</footer>
 	);
